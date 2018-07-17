3ea657b74dee21e3126763b2a102749a999cd9c5
Fernanda Caroline <fe.caroline90@gmail.com>
2018-07-17 09:46:25 -0300

    js

--- js/app.js
+++ js/app.js
@@ -3,4 +3,18 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+// console.log(data);
+
+function loadCoders(turma) {
+  var sede = "CDMX";
+  var listCoders = document.getElementById("alunas-mexico");
+  listCoders.innerHTML = "";
+  for (turma in data[sede]) {
+      for (i in data[sede][turma]["students"]) {
+        console.log(data[sede][turma]);
+        var img = document.createElement("img");
+        img.src = data[sede][turma]["students"][i]["photo"];
+        listCoders.appendChild(img);
+      }
+    }
+};
